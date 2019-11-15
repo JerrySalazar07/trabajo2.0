97a65628f2eb7b7c12ff37bec4624e2fff555772
jpgzuck <50914051+jpgzuck@users.noreply.github.com>
2019-11-15 07:14:00 -0500

    Add files via upload

--- Ejemplo2.py
+++ Ejemplo2.py
@@ -2,17 +2,25 @@ import tkinter as tk
 from tkinter import *
 from random import randint as r
 from tkinter import ttk, messagebox
+from tkinter import PhotoImage
 window = Tk()
 window.geometry("300x350")
+window.config(bg="#181446")
+miFrame = Frame()
+miFrame.pack()
+miFrame.config(bg="red")
+miFrame.config(width="150",height="150")
+miFrame.config(bd="30")
+miFrame.config(relief="sunken")
 aEntrada = tk.StringVar()
 nEntrada = tk.StringVar()
 var = tk.StringVar()
 #imagen = PhotoImage(file="pana.pgm")
 #fondo = Label(window, image=imagen).place(x=20,y=0)
-titulo = Label(window, text="Países participantes", width=15,font=("Arial",20,"bold")).place(x=20,y=140)
-Pas = Label(window, text="Ingrese cantidad de Países participantes: ",font=("Arial",10, "bold")).place(x=20, y=170)
+titulo = Label(window, text="Países participantes", width=15,font=("Comic Sans MS",20,"bold"),bg="#181446",fg="white").place(x=20,y=140)
+Pas = Label(window, text="Ingrese cantidad de Países participantes: ",font=("Comic Sans MS",10, "bold"),bg="#181446",fg="white").place(x=20, y=180)
 nPaises = IntVar()
-Pais = Entry(window,textvariable=nPaises).place(x=80, y=190)
+Pais = Entry(window,textvariable=nPaises).place(x=80, y=210)
 nomPais= tk.StringVar()
 d = tk.getint()
 mPaises=[]
@@ -20,7 +28,7 @@ def pRegistro():
     if nPaises.get()>=10:
        window4 = Toplevel(window)
        window4.geometry("400x300")
-       Label(window4, text="Registro").place(x=50,y=0)
+       Label(window4, text="Registro").place(x=50,y=20)
        
        Entry(window4,textvariable=nomPais, width=20).place(x=50,y=50)
        Button(window4,text="Ingresar", command=RegistrarPais).place(x=70,y=70)
@@ -45,6 +53,7 @@ def vRegistro():
     c=0
     for i in opciones:
        c=c+1
+       
     if c==nPaises.get():
        window2 = Toplevel()
        window2.geometry("500x350")
@@ -60,16 +69,16 @@ def vRegistro():
        opcion = tk.OptionMenu(window2 ,var,*opciones).place(x=100, y=210)
       
        login = Button(window2, text="  Ingresar  ", background="red", command=bLogin).place(x=20, y=300)
-       resultado = Button(window2, text="Resultado", background="red", command=vResultado).place(x=60, y=350)
-       quit = Button(window2, text="     Salir     ", background="red", command=window2.destroy).place(x=100, y=300)
+       resultado = Button(window2, text="Resultado", background="red", command=vResultado).place(x=100, y=300)
+       quit = Button(window2, text="     Salir     ", background="red", command=window2.destroy).place(x=180, y=300)
        for i in range(nPaises.get()):
            mPaises.append([0]*3)
        window2.mainloop() 
     else:
        messagebox.showinfo("Error","Ingresar más países")    
     
-Button(window, text="Aceptar",bg="red", command=pRegistro, width=10).place(x=100,y=220)
-Button(window, text="Salir",bg="red",command=window.destroy,width=10).place(x=100,y=250)
+Button(window, text="Aceptar",bg="red", command=pRegistro, width=10).place(x=100,y=240)
+Button(window, text="Salir",bg="red",command=window.destroy,width=10).place(x=100,y=270)
 nombreC = []
 
      
@@ -129,12 +138,14 @@ def vResultado():
         Label(window3, text=i+1).place(x=0, y=c)
     #iPeru = PhotoImage(file="peru1.pgm")
     #Peru = Label(window3, image=iPeru, text="Perú").place(x=100,y=150)
-    Label(window3, text="Posición").place(x=0,y=0)
-    Label(window3, text="País").place(x=80,y=0)
-    Label(window3, text="   ",background="#CD7F32").place(x=180,y=0)
-    Label(window3, text="   ",background="silver").place(x=200,y=0)
-    Label(window3, text="   ",background="gold").place(x=220,y=0)
-    Label(window3,text="Total").place(x=300,y=0)
+    l1 = Label(window3, text="Posición").place(x=0,y=0)
+    l2 = Label(window3, text="País").place(x=80,y=0)
+    l3 = Label(window3, text="   ",background="#CD7F32").place(x=180,y=0)
+    l4 = Label(window3, text="   ",background="silver").place(x=200,y=0)
+    l5 = Label(window3, text="   ",background="gold").place(x=220,y=0)
+    l6 = Label(window3,text="Total").place(x=300,y=0)
     Button(window3, text="Cerrar",background="red",command=window3.destroy).place(x=200,y=300)
+    
+   
     window3.mainloop()
 window.mainloop()
