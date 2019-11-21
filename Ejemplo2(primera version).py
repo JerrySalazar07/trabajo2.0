a9fc77ea2b9342763528c1234279a20353e75042
JerrySalazar07 <56902411+JerrySalazar07@users.noreply.github.com>
2019-11-20 22:26:05 -0500

    V19

--- Ejemplo2(primera version).py
+++ Ejemplo2(primera version).py
@@ -2,6 +2,8 @@ import tkinter as tk
 from tkinter import *
 from random import randint as r
 from tkinter import ttk, messagebox
+import matplotlib.pyplot as plt
+import numpy as np
 window = Tk()
 window.geometry("300x350")
 aEntrada = tk.StringVar()
@@ -17,7 +19,7 @@ nomPais= tk.StringVar()
 d = tk.getint()
 mPaises=[]
 def pRegistro():
-    if nPaises.get()>=10:
+    if nPaises.get()>=1:
        window4 = Toplevel(window)
        window4.geometry("400x300")
        Label(window4, text="Registro").place(x=50,y=0)
@@ -46,22 +48,23 @@ def vRegistro():
     for i in opciones:
        c=c+1
     if c==nPaises.get():
+       var.set("Elegir pais")
        window2 = Toplevel()
-       window2.geometry("500x350")
+       window2.geometry("350x400")
        #imagen = PhotoImage(file="javelinfinal.pgm")
        #fondo = Label(window2, image=imagen).place(x=0, y=0)
-       titulo = Label(window2, text="Datos del Deportista",font=("Arial",20,"bold")).place(x=20,y=100)
-       nombre = Label(window2, text="Nombres",font=("Arial",10,),background="grey").place(x=20,y=150)
-       apellidos = Label(window2, text="Apellidos",font=("Arial",10,),background="grey").place(x=20, y=180)
-       paises = Label(window2, text="Paises   ",font=("Arial",10),background="grey").place(x=20,y=210)
-       tk.Entry(window2, textvariable=nEntrada).place(x=100,y=150)
-       tk.Entry(window2, textvariable=aEntrada).place(x=100,y=180)
+       titulo = Label(window2, text="Datos del Deportista",font=("Arial",20,"bold")).place(x=40,y=100)
+       nombre = Label(window2, text="Nombres",font=("Arial",10,),background="grey").place(x=40,y=150)
+       apellidos = Label(window2, text="Apellidos",font=("Arial",10,),background="grey").place(x=40, y=180)
+       paises = Label(window2, text="Paises   ",font=("Arial",10),background="grey").place(x=40,y=210)
+       tk.Entry(window2, textvariable=nEntrada).place(x=120,y=150)
+       tk.Entry(window2, textvariable=aEntrada).place(x=120,y=180)
        
-       opcion = tk.OptionMenu(window2 ,var,*opciones).place(x=100, y=210)
+       opcion = tk.OptionMenu(window2 ,var,*opciones).place(x=120, y=210)
       
-       login = Button(window2, text="  Ingresar  ", background="red", command=bLogin).place(x=20, y=300)
-       resultado = Button(window2, text="Resultado", background="red", command=vResultado).place(x=60, y=350)
-       quit = Button(window2, text="     Salir     ", background="red", command=window2.destroy).place(x=100, y=300)
+       login = Button(window2, text="  Ingresar  ", background="red", command=bLogin).place(x=60, y=300)
+       resultado = Button(window2, text="Resultado", background="red", command=vResultado).place(x=80, y=350)
+       quit = Button(window2, text="     Salir     ", background="red", command=window2.destroy).place(x=130, y=300)
        for i in range(nPaises.get()):
            mPaises.append([0]*3)
        window2.mainloop() 
@@ -115,7 +118,7 @@ def vResultado():
     window3.geometry("500x350")
     suma=0
     sumaT=[]
-     #a=0
+    a=0
     c=0
     for i in range(nPaises.get()):
         for j in range(3):
@@ -127,7 +130,7 @@ def vResultado():
     for i in range(nPaises.get()):
         c=c+30
         Label(window3, text=i+1).place(x=0, y=c)
-        
+
     print("Pais\t\tMedallas\t\tTotal")
     for i in range(len(mPaises)):
         for j in range(len(mPaises)-1):
@@ -156,5 +159,13 @@ def vResultado():
     Label(window3, text="   ",background="gold").place(x=220,y=0)
     Label(window3,text="Total").place(x=300,y=0)
     Button(window3, text="Cerrar",background="red",command=window3.destroy).place(x=200,y=300)
+    for i in range(len(mPaises)):
+        f=160
+        a=a+30
+        Label(window3, text=opciones[i]).place(x=80,y=a)
+        Label(window3,text=sumaT[i]).place(x=300,y=a)
+        for j in range(3):
+            f=f+20
+            Label(window3, text=mPaises[i][j]).place(x=f,y=a)
     window3.mainloop()
 window.mainloop()
